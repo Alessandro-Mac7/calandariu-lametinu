992f9ec0099c2259bd23fa9c7cacdec74918c526
alessandro.macri <>
2025-10-01 20:33:06 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/calandariu-lametinu/sw.js', { scope: '/calandariu-lametinu/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
